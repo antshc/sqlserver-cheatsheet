e5072cf53d4b5a20ce45cd8b0a710452fc7cb531
shcherby <anton.shcherbyna@hpe.com>
2025-07-15 16:54:41 +0200

    Update

--- EfSqlPerformance/EfSqlPerformance.Test/UsersQueryTest.cs
+++ EfSqlPerformance/EfSqlPerformance.Test/UsersQueryTest.cs
@@ -1,22 +1,41 @@
+using EfSqlPerformance.Api.Data;
 using EfSqlPerformance.Test.Fixtures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EfSqlPerformance.Test
 {
-    public class UsersQueryTest : QueryTestBase, IClassFixture<DbContextFixture>
+    public class UsersQueryTest
     {
-        public UsersQueryTest(DbContextFixture contextFixture, ITestOutputHelper output)
-            : base(contextFixture, output)
+        private readonly ITestOutputHelper _output;
+        private const string ConnectionString = "Server=localhost,14330;Database=StackOverflow2010;User Id=sa;Password=yourStrong(!)Password;TrustServerCertificate=True;";
+
+        public UsersQueryTest(ITestOutputHelper output)
         {
+            _output = output;
         }
 
         [Fact]
         public void GetTopUsersTest()
         {
-            var topUsers = Context.Users
-                .Where(u => u.Reputation > 1000)
-                .OrderByDescending(u => u.Reputation)
-                .Take(10)
-                .ToList();
+            Parallel.ForEach(
+                Enumerable.Range(0, 10000),
+                new ParallelOptions { MaxDegreeOfParallelism = 10 },
+                _ =>
+                {
+                    var options = new DbContextOptionsBuilder<StackOverflowContext>()
+                        .UseSqlServer(ConnectionString)
+                        .LogTo(_output.WriteLine, LogLevel.Information) // Logs to xUnit
+                        .EnableSensitiveDataLogging() // Shows parameter values
+                        .Options;
+
+                    using var context = new StackOverflowContext(options);
+                    context.Users
+                        .Where(u => u.Reputation > 1000)
+                        .OrderByDescending(u => u.Reputation)
+                        .Take(1000)
+                        .ToList();
+                });
         }
     }
 }
